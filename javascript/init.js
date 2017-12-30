abd4831f7a5be21178198bff333a9aeb022028df
rachen <anelhaman@hotmail.com>
2017-12-30 23:08:34 +0700

    add progress bar

--- javascript/init.js
+++ javascript/init.js
@@ -1,1 +1,47 @@
-console.log("OK")
+$(document).ready(()=>{
+  $progressbar = $("#progressbar")
+  
+  $progressbar.fadeIn();
+	$progressbar.width('0%');
+	$progressbar.animate({width:'100%'},200);
+  $progressbar.fadeOut();
+  
+  $("#btnSubmit").on("click",()=>{
+    $progressbar.fadeIn();
+    $progressbar.width('0%');
+    $progressbar.animate({width:'30%'},600,e=>{
+      $progressbar.animate({width:'70%'},200,e=>{
+        $progressbar.animate({width:'100%'},75,e=>{
+        $progressbar.fadeOut();
+        })
+      })
+    })
+    
+    
+    // alert("Saved")
+  })
+  $("#btnClear").on("click",()=>{
+    $progressbar.fadeIn();
+    $progressbar.width('100%');
+    $progressbar.animate({width:'85%'},500,e=>{
+      $progressbar.animate({width:'70%'},1000,e=>{
+        $progressbar.animate({width:'0%'},150,e=>{
+        
+          $progressbar.fadeIn();
+          $progressbar.width('0%');
+          
+              $progressbar.animate({width:'100%'},150,e=>{
+                $progressbar.fadeOut();
+            
+          })
+          
+        
+         
+        })
+      })
+    })
+
+   
+    // alert("Clear")
+  })
+})
